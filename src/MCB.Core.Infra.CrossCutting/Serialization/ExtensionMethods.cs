f633ac5f19a8b22e4f0448570171c39a0878d4bd
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2022-04-22 20:11:08 -0300

    feat: add serialization extension methods

---
+++ src/MCB.Core.Infra.CrossCutting/Serialization/ExtensionMethods.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+
+namespace MCB.Core.Infra.CrossCutting.Serialization
+{
+    public static class ExtensionMethods
+    {
+        // Fields
+        private static JsonSerializerOptions _jsonSerializerOptions;
+        private static Newtonsoft.Json.Schema.Generation.JSchemaGenerator _jSchemaGenerator;
+
+        // Constructors
+        static ExtensionMethods()
+        {
+            _jsonSerializerOptions = new JsonSerializerOptions(new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+            _jSchemaGenerator = new Newtonsoft.Json.Schema.Generation.JSchemaGenerator();
+        }
+
+        // Public Methods
+        public static string SerializeToJson(this object obj)
+        {
+            return JsonSerializer.Serialize(obj, _jsonSerializerOptions);
+        }
+        public static T? DeserializeFromJson<T>(this string json)
+        {
+            return JsonSerializer.Deserialize<T>(json, _jsonSerializerOptions);
+        }
+        public static string GenerateJsonSchema(this Type type)
+        {
+            return _jSchemaGenerator.Generate(type).ToString();
+        }
+        public static string GenerateJsonSchema(this object obj)
+        {
+            return GenerateJsonSchema(obj.GetType());
+        }
+    }
+}
