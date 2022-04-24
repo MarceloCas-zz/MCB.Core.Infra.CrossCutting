c978a01904d4401e83b8b5fe5268785a4684c93a
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2022-04-23 23:50:42 -0300

    #feat: add TimeProvider

---
+++ src/MCB.Core.Infra.CrossCutting/Time/TimeProvider.cs
@@ -0,0 +1,14 @@
+﻿namespace MCB.Core.Infra.CrossCutting.Time
+{
+    public static class TimeProvider
+    {
+        // Properties
+        public static DateTimeOffset? InjectedUtcNow { get; set; }
+
+        // Public Methods
+        public static DateTimeOffset GetUtcNow()
+        {
+            return InjectedUtcNow ?? DateTimeOffset.UtcNow;
+        }
+    }
+}
---
+++ tst/MCB.Core.Infra.CrossCutting.Tests/TimeTests/TimeProviderTest.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using MCB.Core.Infra.CrossCutting.Time;
+using System;
+using Xunit;
+
+namespace MCB.Core.Infra.CrossCutting.Tests.TimeTests
+{
+    public class TimeProviderTest
+    {
+        [Fact]
+        public void TimeProvider_Should_Return_GetUtcNow()
+        {
+            // Arrange
+            var utcNow = DateTimeOffset.UtcNow;
+
+            // Act
+            var timeProviderUtcNow = TimeProvider.GetUtcNow();
+
+            // Assert
+            timeProviderUtcNow.Should().BeAfter(utcNow);
+        }
+        [Fact]
+        public void TimeProvider_Should_Inject_UtcNow()
+        {
+            // Arrange
+            var utcNow = DateTimeOffset.UtcNow;
+
+            // Act
+            TimeProvider.InjectedUtcNow = utcNow;
+            var timeProviderUtcNow = TimeProvider.GetUtcNow();
+
+            // Assert
+            timeProviderUtcNow.Should().Be(utcNow);
+        }
+    }
+}
