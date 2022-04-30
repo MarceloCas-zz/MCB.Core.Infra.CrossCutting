68c0e9014ca0785f5b21aefa4cee3b38a6b36e88
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2022-04-29 23:17:40 -0300

    feat: add enum utils

---
+++ tst/MCB.Core.Infra.CrossCutting.Tests/EnumsTests/EnumUtilsTest.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using MCB.Core.Infra.CrossCutting.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace MCB.Core.Infra.CrossCutting.Tests.EnumsTests
+{
+    public class EnumUtilsTest
+    {
+        [Fact]
+        public void EnumUtils_Should_GetRandomEnumValue()
+        {
+            // Arrange
+            var dummyEnumCollection = new List<DummyEnum>();
+
+            // Act
+            for (int i = 0; i < 100; i++)
+                dummyEnumCollection.Add(EnumUtils.GetRandomEnumValue<DummyEnum>());
+
+            // Assert
+            foreach (var dummyEnum in dummyEnumCollection)
+                Enum.IsDefined(dummyEnum).Should().BeTrue();
+
+            dummyEnumCollection.Distinct().Count().Should().BeGreaterThan(1);
+        }
+
+        public enum DummyEnum
+        {
+            DummyValueA,
+            DummyValueB,
+            DummyValueC
+        }
+    }
+}
