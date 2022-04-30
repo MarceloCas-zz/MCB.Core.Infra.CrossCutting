fbebc8eb52b319bc3b98b8942242e1a1eac5333e
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2022-04-29 23:18:08 -0300

    refact: rename TimeProvider to DateTimeProvider

---
+++ src/MCB.Core.Infra.CrossCutting/DateTime/DateTimeProvider.cs
@@ -0,0 +1,14 @@
+﻿namespace MCB.Core.Infra.CrossCutting.DateTime
+{
+    public static class DateTimeProvider
+    {
+        // Properties
+        public static Func<DateTimeOffset>? GetDateCustomFunction { get; set; }
+
+        // Public Methods
+        public static DateTimeOffset GetDate()
+        {
+            return GetDateCustomFunction?.Invoke() ?? DateTimeOffset.UtcNow;
+        }
+    }
+}
---
+++ tst/MCB.Core.Infra.CrossCutting.Tests/DateTimeTests/DateTimeProviderTest.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using MCB.Core.Infra.CrossCutting.DateTime;
+using System;
+using Xunit;
+
+namespace MCB.Core.Infra.CrossCutting.Tests.DateTimeTests
+{
+    public class DateTimeProviderTest
+    {
+        [Fact]
+        public void DateTimeProvider_Should_Return_GetDate()
+        {
+            // Arrange
+            var utcNow = DateTimeOffset.UtcNow;
+
+            // Act
+            var timeProviderUtcNow = DateTimeProvider.GetDate();
+
+            // Assert
+            timeProviderUtcNow.Should().BeAfter(utcNow);
+        }
+        [Fact]
+        public void DateTimeProvider_Should_Return_GetDateCustomFuncion()
+        {
+            // Arrange
+            var utcNow = DateTimeOffset.UtcNow;
+
+            // Act
+            DateTimeProvider.GetDateCustomFunction = new Func<DateTimeOffset>(() => utcNow);
+            var timeProviderUtcNow = DateTimeProvider.GetDate();
+
+            // Assert
+            timeProviderUtcNow.Should().Be(utcNow);
+        }
+    }
+}
